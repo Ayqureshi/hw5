fe521e706189d1fa60710ec890d825dc0964db7c
Ameen Qureshi <auquresh@usc.edu>
2023-04-06 18:15:11 +0000

    Final

--- schedwork.cpp
+++ schedwork.cpp
@@ -20,7 +20,8 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool helper(const AvailabilityMatrix& avail,const unsigned int& w_availability, const unsigned int& shift_max, const unsigned int& day_need, const unsigned int& day_period, DailySchedule& sched, unsigned int row, unsigned int col);
+bool valid(const AvailabilityMatrix& avail,const unsigned int& shift_max, const unsigned int& day_need, const unsigned int& day_period, const DailySchedule& sched, const Worker_T& worker,unsigned int row, unsigned int col, unsigned int counter);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -36,9 +37,67 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    unsigned int w_availability = avail[0].size();//k
+    unsigned int day_period = avail.size();// n
+    unsigned int day_need = dailyNeed;//d
+    unsigned int shift_max = maxShifts; //m
+    unsigned int r = 0; 
+    unsigned int c = 0; 
 
-
-
-
+    for(unsigned int i = 0;i < day_period;i++){
+        vector<Worker_T> container;
+        sched.push_back(container);
+        for(unsigned int l =0; l <day_need;l++){
+            sched[i].push_back(INVALID_ID);
+        }
+		}
+		
+		bool retval;
+    return retval = helper(avail, w_availability, shift_max, day_need, day_period, sched,r,c);
+         
 }
 
+bool helper(const AvailabilityMatrix& avail,const unsigned int& w_availability, const unsigned int& shift_max, const unsigned int& day_need, const unsigned int& day_period, DailySchedule& sched, unsigned int row, unsigned int col){
+	if(row == day_period-1){
+		if(col == day_need){
+			return true;
+		}
+	}
+	if(col == day_need){
+		row++;
+		col = 0; 
+	}
+	for(Worker_T i=0; i <Worker_T(w_availability);i++){
+		sched[row][col] = i;
+		if(valid(avail, shift_max, day_need, day_period, sched,i, 0, 0, 0)==true){
+			if(helper(avail, w_availability, shift_max, day_need, day_period, sched,row,col+1)==true){
+				return true;
+			}
+		}
+		else if (valid(avail, shift_max, day_need, day_period, sched,i, 0, 0, 0)==false || helper(avail, w_availability, shift_max, day_need, day_period, sched,row,col+1)==false){
+			sched[row][col] = INVALID_ID;	
+		}
+	}
+	return false;
+}
+bool valid(const AvailabilityMatrix& avail,const unsigned int& shift_max, const unsigned int& day_need, const unsigned int& day_period, const DailySchedule& sched, const Worker_T& worker, unsigned int row, unsigned int col,unsigned int counter){
+	if(col >= day_need){
+		row++;
+		col = 0;
+	}
+	if(row >= day_period){
+		return true;
+	}
+	if(sched[row][col] == worker){
+				counter++;
+				// go to the next day 
+				if(avail[row][worker]==false){
+					//if the matrix when factoring in the id is false return false
+					return false;
+				}
+	}
+	if(counter > shift_max){
+		return false;
+	}
+	return valid(avail, shift_max, day_need, day_period, sched, worker, row, col+1, counter);				
+}
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void wordlehelper(std::set<std::string>& results, unsigned int position, unsigned int empty, std::string& inn,std::string& floatin, const std::set<std::string>& dict);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +22,66 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+		std::set<std::string> results;
+		std::string inn = in;
+		std::string floatin = floating; 
+		unsigned int position = 0;
+		unsigned int fred = 0;
+		for(unsigned int i =0; i< inn.size();i++){
+			if( inn[i] == '-'){
+				fred++;
+			}
+		}
+		wordlehelper(results, position, fred, inn, floatin, dict);
+		return results;
 }
 
 // Define any helper functions here
+void wordlehelper(std::set<std::string>& results, unsigned int position, unsigned int fred, std::string& inn,std::string& floatin, const std::set<std::string>& dict){
+	if(position == inn.length()){
+		if(dict.find(inn) == dict.end()){
+			return;
+		}
+		else if(dict.find(inn) != dict.end()){
+			results.insert(inn);
+			return;
+		}		
+	}
+	else if(inn[position] != '-'){
+		wordlehelper(results, position+1, fred, inn, floatin, dict);
+		return;
+	}
+	else if(fred <= floatin.length()){
+		if(floatin.length() > 0){
+			for(auto i:floatin){
+				std::string floaty = floatin;
+				inn[position] = i;
+				char eraser = floatin.find(i);
+				floatin.erase(eraser,1);
+				wordlehelper(results,position+1, fred-1,inn, floatin, dict);
+				floatin = floaty;
+			}
+			inn[position] = '-';
+		}
+	}
+	else{
+			for(char i = 'a'; i <= 'z';i++){
+				inn[position] = i;
+				if(floatin.find(i) != floatin.npos){
+					std::string floaty = floatin;
+					inn[position] = i;
+					char eraser = floatin.find(i);
+					floatin.erase(eraser,1);
+					wordlehelper(results,position+1, fred-1,inn, floatin, dict);
+					floatin = floaty;
+				}
+				else if(floatin.find(i) == floatin.npos){
+					// position++;
+					wordlehelper(results,position+1, fred-1,inn, floatin, dict);
+				}
+			}
+			inn[position] = '-';
+		}
+	return;
+	}
+	
